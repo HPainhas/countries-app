7142f002a8ba1e0a18fb37cd10653f765fb0b0b9
HPainhas <hpainhas@hotmail.com>
2021-09-08 20:28:03 -0400

    Set up unit tests.

---
+++ app/src/test/java/com/example/countriesapp/ListViewModelUnitTest.java
@@ -0,0 +1,35 @@
+package com.example.countriesapp;
+
+import androidx.annotation.NonNull;
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.internal.schedulers.ExecutorScheduler;
+import io.reactivex.plugins.RxJavaPlugins;
+
+public class ListViewModelUnitTest
+{
+    @Rule
+    public InstantTaskExecutorRule rule = new InstantTaskExecutorRule();
+
+    @Before
+    public void setupRxSchedulers()
+    {
+        Scheduler immediate = new Scheduler()
+        {
+            @NonNull
+            @Override
+            public Worker createWorker()
+            {
+                return new ExecutorScheduler.ExecutorWorker(Runnable::run, true);
+            }
+        };
+
+        RxJavaPlugins.setInitNewThreadSchedulerHandler(scheduler -> immediate);
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(scheduler -> immediate);
+    }
+}
